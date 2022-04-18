ba4d735bba9a5853603c91fbde590c59fc899140
Rahul Sharma <58264599+OrneyVortex@users.noreply.github.com>
2022-04-18 23:26:48 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,28 @@
+import React, {useState} from 'react'
 import  './App.css';
- import About from './components/About';
+//  import About from './components/About';
 import Navbar from './components/Navbar';
-// import TextForm from './components/TextForm';
+import TextForm from './components/TextForm';
 
 function App() {
+  const [mode, setMode] = useState('light');
+
+  const toggleMode=() =>{
+    if(mode==='light'){
+      setMode('dark')
+      document.body.style.backgroundColor='#05103A'
+    }
+    else{
+      setMode('light')
+      document.body.style.backgroundColor='white'
+    }
+  }
   return (
     <>
-      <Navbar title="TextUtils" />
-      <div className="container">
-       <About/>
-      {/* <TextForm heading ="Enter your text to analyze"/> */}
+      <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode} />
+      <div className="container my-3">
+       {/* <About/> */}
+      <TextForm heading ="Enter your text to analyze" mode={mode}/>
       </div>
     </>
   );

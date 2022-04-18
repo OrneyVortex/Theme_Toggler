b54afc8e41fd4d47531b151d9245a2c17be0b4e3
Rahul Sharma <58264599+OrneyVortex@users.noreply.github.com>
2022-04-18 23:27:12 +0530

    Update TextForm.js

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -3,6 +3,7 @@ import React, { useState } from 'react'
 export default function TextForm(props) {
     const [text,setText]= useState("");
 
+
     const handleUpClick = () =>{
         console.log("Uppercase was clicked");
         let newText=text.toUpperCase();
@@ -22,16 +23,16 @@ export default function TextForm(props) {
     }
     return (
         <>
-        <div className='container'>
-            <h1>{props.heading}</h1>
+        <div className='container' style={{color: props.mode==='dark'?'white':'black'}}>
+            <h1 >{props.heading}</h1>
             <div className="mb-3">
-                <textarea className="form-control" value={text} onChange={handleOnChange} id="myBox" rows="8"></textarea>
+                <textarea className="form-control" value={text} onChange={handleOnChange} id="myBox" rows="8" style={{backgroundColor: props.mode==='dark'?'grey':'white' , color: props.mode==='dark'?'white':'black'}}></textarea>
             </div>  
             <button className="btn btn-primary" onClick={handleUpClick}>Convert to Uppercase</button>
             <button className="btn btn-primary mx-2" onClick={handleLoClick}>Convert to Lowercase</button>
             <button className="btn btn-primary mx-2" onClick={handleClearClick}>Clear All</button>
         </div>
-        <div className="container my-4">
+        <div className="container my-4" style={{color: props.mode==='dark'?'white':'black'}}>
             <h1>Your text summary</h1>
             <p>{text.length} characters</p> 
             <p>{text.split(" ").length} words</p> 
